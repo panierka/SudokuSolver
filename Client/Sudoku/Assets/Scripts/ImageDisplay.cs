0dc39fc132ed11651d1b59c8d2e62e2d22ba8550
TR1PL3D0T <hubikwiatkowski@gmail.com>
2025-01-26 03:53:55 +0100

    feat: waiting scene image size

--- Client/Sudoku/Assets/Scripts/ImageDisplay.cs
+++ Client/Sudoku/Assets/Scripts/ImageDisplay.cs
@@ -2,14 +2,19 @@ using Assets.Scripts.Utils;
 using Assets.Scripts;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ImageDisplay : MonoBehaviour
 {
+	public Image targetImage;
 	void Start()
 	{
 		Texture2D texture = AppFlowDao.Instance.CurrentSentTexture;
 		Rect rect = new Rect(0, 0, texture.width, texture.height);
 		Vector2 pivot = new Vector2(0.5f, 0.5f);
-		GetComponent<SpriteRenderer>().sprite = Sprite.Create(texture, rect, pivot);
+		targetImage.GetComponent<RectTransform>().sizeDelta = new Vector2(texture.width, texture.height);
+		targetImage.sprite = Sprite.Create(texture, rect, pivot);
+		Debug.Log($"texture.width: {texture.width}");
+		Debug.Log($"texture.height: {texture.height}");
 	}
 }
