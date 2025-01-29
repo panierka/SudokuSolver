acf5d39f3575ed3e2b6f6987cb1fd8c1949c204b
jdylik <jan.karol.dylik@gmail.com>
2025-01-29 07:04:51 +0100

    Digit recognizer - file consolidation - first iteration

--- Server/SudokuServer/detector.py
+++ Server/SudokuServer/detector.py
@@ -1,6 +1,7 @@
 import random
-
-
+from SudokuRecognizer.sent_digits_recognizer import main
+#lista obrazów nie lepsza żeby tylko raz wczytać model?
 def detect_digit(im):
     # tutaj powinien być użyty model
+    main(im)
     return 0
